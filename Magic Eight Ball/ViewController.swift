5d6e8921e98fd0ccee3d6a1da76f487a97a14c33
rudiwijaya <rudi.wijaya@mitrais.com>
2019-02-19 09:31:25 +0700

    done

--- Magic Eight Ball/ViewController.swift
+++ Magic Eight Ball/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ball: UIImageView!
+    
+    let answers: [String] = ["ball1", "ball2", "ball3", "ball4", "ball5"];
+    
+    var valueAnswer: Int = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        randomAndUpdateBall();
+    }
+    
+    @IBAction func btnAskPressed(_ sender: Any) {
+        randomAndUpdateBall();
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if (motion == UIEvent.EventSubtype.motionShake) {
+            randomAndUpdateBall();
+        }
+    }
+    
+    func randomAndUpdateBall() {
+        valueAnswer = Int.random(in: 0...4);
+        
+        ball.image = UIImage(named: answers[valueAnswer])
     }
 
 
